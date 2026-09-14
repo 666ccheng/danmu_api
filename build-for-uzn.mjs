ad5be993c2f3ab76abd1f506d963be26f6630544
666ccheng <3036015910@qq.com>
2026-09-14 10:44:29 +0800

    Update build-for-uzn.mjs
    Ok

--- build-for-uzn.mjs
+++ build-for-uzn.mjs
@@ -1,10 +1,21 @@
+// build-for-uzn.mjs
+import { mkdirSync, writeFileSync, readFileSync } from 'fs'
 import { build } from 'esbuild'
+import crypto from 'crypto'
+
+mkdirSync('./dist', { recursive: true })
 
 await build({
-  entryPoints: ['./danmu_api/index.js'],
-  outfile: 'danmu_api_server.cjs',
+  // ✅ 入口已经写死 danmu_api/server.js
+  entryPoints: ['./danmu_api/server.js'],
+  outfile: './dist/danmu_api_server.cjs',
   bundle: true,
   platform: 'node',
   format: 'cjs',
-  minify: true
+  external: [],
 })
+
+const buf = readFileSync('./dist/danmu_api_server.cjs')
+const md5 = crypto.createHash('md5').update(buf).digest('hex')
+writeFileSync('./dist/danmu_api_server.cjs.md5', md5)
+console.log('✅打包完成，md5:', md5)
