19d1637bb546730d8b5b74ea4b5153a62a357802
666ccheng <3036015910@qq.com>
2026-09-14 10:59:44 +0800

    Update build-for-uzn.mjs

--- build-for-uzn.mjs
+++ build-for-uzn.mjs
@@ -6,16 +6,22 @@ import crypto from 'crypto'
 mkdirSync('./dist', { recursive: true })
 
 await build({
-  // ✅ 入口已经写死 danmu_api/server.js
   entryPoints: ['./danmu_api/server.js'],
   outfile: './dist/danmu_api_server.cjs',
   bundle: true,
   platform: 'node',
   format: 'cjs',
+  // 核心！套async自执行函数，解决Top-level await报错，就是别人打包cjs的方案
+  banner: {
+    js: '(async () => {'
+  },
+  footer: {
+    js: '})();'
+  },
   external: [],
 })
 
 const buf = readFileSync('./dist/danmu_api_server.cjs')
 const md5 = crypto.createHash('md5').update(buf).digest('hex')
 writeFileSync('./dist/danmu_api_server.cjs.md5', md5)
-console.log('✅打包完成，md5:', md5)
+console.log('✅打包cjs完成，md5:', md5)
